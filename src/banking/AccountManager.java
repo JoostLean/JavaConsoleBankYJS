b3c7185a1df3175336b87290dba05d5d7428a2b3
JoostLean <jusang2@gmail.com>
2024-01-18 18:18:59 +0900

    2024/01/18 -

--- src/banking/AccountManager.java
+++ src/banking/AccountManager.java
@@ -1,12 +1,57 @@
 package banking;
 
 public class AccountManager {
-	private String accNum;
-	private String name;
-	private int balance;
-	public AccountManager(String accNum, String name, int balance) {
-		this.accNum = accNum;
-		this.name = name;
-		this.balance = balance;
+//	private String accNum;
+//	private String name;
+//	private int balance;
+//	public AccountManager(String accNum, String name, int balance) {
+//		this.accNum = accNum;
+//		this.name = name;
+//		this.balance = balance;
+//	}
+	public AccountManager() {
+		super();
+	}
+	void showMenu() {
+		System.out.println("-----Menu-----");
+		System.out.print("1.계좌개설");
+		System.out.print("   ");
+		System.out.print("2.입금");
+		System.out.print("   ");
+		System.out.println("3.출금");
+		System.out.print("4.계좌정보출력");
+		System.out.print("  ");
+		System.out.println("5.프로그램종료");
+		System.out.print("선택:");
+	}
+	void makeAccount() {
+		System.out.println("***신규계좌개설***");
+		System.out.println("계좌번호 : ");
+		System.out.println("고객이름 : ");
+		System.out.println("잔고 : ");
+		System.out.println("계좌개설이 완료되었습니다.");
+	}
+	void depositMoney() {
+		System.out.println("*****입  금*****");
+		System.out.println("계좌번호와 입금할 금액을 입력하세요");
+		System.out.println("계좌번호:");
+		System.out.println("입금액:");
+		System.out.println("입금이 완료되었습니다.");
+	}
+	void withdrawMoney() {
+		System.out.println("*****출  금*****");
+		System.out.println("계좌번호와 출금할 금액을 입력하세요");
+		System.out.println("계좌번호:");
+		System.out.println("입금액:");
+		System.out.println("출금이 완료되었습니다.");
+	}
+	void showAccInfo() {
+		System.out.println("***계좌정보출력***");
+		System.out.println("-------------");
+		System.out.println("계좌번호 : ");
+		System.out.println("고객이름 : ");
+		System.out.println("잔고 : ");
+		System.out.println("-------------");
+		System.out.println("전체계좌정보 출력이 완료되었습니다.");
 	}
 }
--- src/banking/BankingSystemMain.java
+++ src/banking/BankingSystemMain.java
@@ -14,48 +14,38 @@ interface BankingSystemMainMenu {
 public class BankingSystemMain implements BankingSystemMainMenu {
 
 	public static void main(String[] args) {
-//		BankingSystemHandler handler = new BankingSystemHandler();
+		BankingSystemHandler handler = new BankingSystemHandler();
 		Scanner scanner = new Scanner(System.in);
 
-		menuSelect();
+		handler.showMenu();
 
 		int choice = scanner.nextInt();
 		switch (choice) {
 			case MAKE:
-				System.out.println("***신규계좌개설***");
+				handler.makeAccount();
 				break;
 			case DEPOSIT:
-				System.out.println("*****입  금*****");
+				handler.depositMoney();
 				break;
 			case WITHDRAW:
-				System.out.println("*****출  금*****");
+				handler.withdrawMoney();
 				break;
 			case INQUIRE:
-				System.out.println("***계좌정보출력***");
+				handler.showAccInfo();
 				break;
 			case EXIT:
 				System.out.println("***프로그램종료***");
+				System.out.println("프로그램이 종료되었습니다.");
 				break;
 			default:
 				System.out.println("메뉴를 선택해주세요.");
-				menuSelect();
+				handler.showMenu();
 		}
-	}
-	
-	static void menuSelect() {
-		System.out.println("-----Menu-----");
-		System.out.print("1.계좌개설");
-		System.out.print("   ");
-		System.out.print("2.입금");
-		System.out.print("   ");
-		System.out.println("3.출금");
-		System.out.print("4.계좌정보출력");
-		System.out.print("  ");
-		System.out.println("5.프로그램종료");
-		System.out.print("선택:");
 	}
 }
 
-//class BankingSystemHandler extends AccountManager {
-//	
-//}
+class BankingSystemHandler extends AccountManager {
+	public BankingSystemHandler() {
+		super();
+	}
+}
