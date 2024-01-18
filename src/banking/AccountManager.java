4a411e0bffd2a6d1113ec03854bbbcf3588691bb
JoostLean <jusang2@gmail.com>
2024-01-18 17:56:03 +0900

    2024/01/18 -

---
+++ src/banking/AccountManager.java
@@ -0,0 +1,12 @@
+package banking;
+
+public class AccountManager {
+	private String accNum;
+	private String name;
+	private int balance;
+	public AccountManager(String accNum, String name, int balance) {
+		this.accNum = accNum;
+		this.name = name;
+		this.balance = balance;
+	}
+}
--- src/banking/BankingSystemMain.java
+++ src/banking/BankingSystemMain.java
@@ -1,13 +1,48 @@
 package banking;
 
 import java.util.Scanner;
+import banking.AccountManager;
 
-public class BankingSystemMain {
+interface BankingSystemMainMenu {
+	final int MAKE = 1;
+	final int DEPOSIT = 2;
+	final int WITHDRAW = 3;
+	final int INQUIRE = 4;
+	final int EXIT = 5;
+}
+
+public class BankingSystemMain implements BankingSystemMainMenu {
 
 	public static void main(String[] args) {
-		BankingSystemHandler handler = new BankingSystemHandler();
+//		BankingSystemHandler handler = new BankingSystemHandler();
 		Scanner scanner = new Scanner(System.in);
 
+		menuSelect();
+
+		int choice = scanner.nextInt();
+		switch (choice) {
+			case MAKE:
+				System.out.println("***신규계좌개설***");
+				break;
+			case DEPOSIT:
+				System.out.println("*****입  금*****");
+				break;
+			case WITHDRAW:
+				System.out.println("*****출  금*****");
+				break;
+			case INQUIRE:
+				System.out.println("***계좌정보출력***");
+				break;
+			case EXIT:
+				System.out.println("***프로그램종료***");
+				break;
+			default:
+				System.out.println("메뉴를 선택해주세요.");
+				menuSelect();
+		}
+	}
+	
+	static void menuSelect() {
 		System.out.println("-----Menu-----");
 		System.out.print("1.계좌개설");
 		System.out.print("   ");
@@ -18,21 +53,9 @@ public class BankingSystemMain {
 		System.out.print("  ");
 		System.out.println("5.프로그램종료");
 		System.out.print("선택:");
-		
-		int choice = scanner.nextInt();
-//		switch (choice) {
-//			case 1:
-//				userResult = "가위"; break;
-//			case 2:
-//				userResult = "바위"; break;
-//			case 3:
-//				userResult = "보"; break;
-//			default:
-//				userResult = "undefined"; break;
-//		}
 	}
 }
 
-class BankingSystemHandler {
-	
-}
+//class BankingSystemHandler extends AccountManager {
+//	
+//}
