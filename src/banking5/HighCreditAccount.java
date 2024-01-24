300b1c82b22d33f42fcae9694fc396a9a46410c0
JoostLean <jusang2@gmail.com>
2024-01-24 11:22:56 +0900

    2024/01/24 - 입출력 스트림 처리, 계좌정보 불러오기/저장

---
+++ src/banking5/HighCreditAccount.java
@@ -0,0 +1,49 @@
+package banking5;
+
+import java.io.Serializable;
+
+public class HighCreditAccount extends Account {
+
+	private int interest;
+	private int plusInterest;
+	private char credit;
+	
+	public HighCreditAccount(String accNum, String name, int balance, int accType, char credit, int interest, int plusInterest) {
+		super(accNum, name, balance, accType);
+		this.interest = interest;
+		this.plusInterest = plusInterest;
+		this.credit = credit;
+	}
+
+	@Override
+	public int getInterest() {
+		return interest;
+	}
+	@Override
+	public void setInterest(int interest) {
+		this.interest = interest;
+	}
+	@Override
+	public int getPlusInterest() {
+		if(credit=='A') {
+			this.plusInterest = 7;
+		}
+		else if(credit=='B') {
+			this.plusInterest = 4;
+		}
+		else if(credit=='C') {
+			this.plusInterest = 2;
+		}
+		return plusInterest;
+	}
+	@Override
+	public void setPlusInterest(int plusInterest) {
+		this.plusInterest = plusInterest;
+	}
+	public char getCredit() {
+		return credit;
+	}
+	public void setCredit(char credit) {
+		this.credit = credit;
+	}
+}
